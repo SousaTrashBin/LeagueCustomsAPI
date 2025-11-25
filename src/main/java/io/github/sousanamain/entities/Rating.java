f1ff66e7643f32f9892139fc3c699c8f7ce69c16
sousanamain <sousanamain@gmail.com>
2025-11-25 05:57:33 +0000

    fix, rating is embeddable

--- src/main/java/io/github/sousanamain/entities/Rating.java
+++ src/main/java/io/github/sousanamain/entities/Rating.java
@@ -2,11 +2,9 @@ package io.github.sousanamain.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.Table;
 import lombok.*;
 
 @Embeddable
-@Table(name = "ratings")
 @Getter
 @Setter
 @NoArgsConstructor
